66680722b4669e7ccb3895d260cca3a744064afb
euntaek <zmn5119@gmail.com>
2020-09-25 18:35:21 +0900

    feat(요청 오류): 오류 위치 필드 추가

--- server/src/errors/errRequest.ts
+++ server/src/errors/errRequest.ts
@@ -2,11 +2,13 @@ import { StatusCodes } from 'http-status-codes';
 
 interface ErrorParmas {
   message?: string;
+  location?: string;
   log?: any;
 }
 export abstract class RequestError extends Error {
   statusCode: number;
-  log: any;
+  location?: string;
+  log?: any;
   constructor(message?: string) {
     super(message);
   }
@@ -15,6 +17,7 @@ export class InternalServerError extends RequestError {
   constructor(errorParams?: ErrorParmas) {
     super(errorParams?.message || '내부 서버 오류');
     this.log = errorParams?.log;
+    this.location = errorParams?.location;
     this.statusCode = StatusCodes.INTERNAL_SERVER_ERROR;
   }
 }
@@ -22,6 +25,7 @@ export class BadRequest extends RequestError {
   constructor(errorParams?: ErrorParmas) {
     super(errorParams?.message || '유효하지 않은 요청');
     this.log = errorParams?.log;
+    this.location = errorParams?.location;
     this.statusCode = StatusCodes.BAD_REQUEST;
   }
 }
@@ -30,6 +34,7 @@ export class NotFound extends RequestError {
   constructor(errorParams?: ErrorParmas) {
     super(errorParams?.message || '리소스가 존재하지 않음');
     this.log = errorParams?.log;
+    this.location = errorParams?.location;
     this.statusCode = StatusCodes.NOT_FOUND;
   }
 }
