f5b0f10bfdff2c461e0a5b3c01cd4578cbef868b
euntaek <zmn5119@gmail.com>
2020-09-26 16:45:08 +0900

    style(middleware/utils): 불필요한 코드 제거

--- server/src/lib/middlewares/utils.ts
+++ server/src/lib/middlewares/utils.ts
@@ -4,10 +4,9 @@ import { ObjectID } from 'mongodb';
 import { BadRequest } from '../../errors/errRequest';
 
 export const checkObjectId: Middleware = (ctx, next) => {
-  console.log('chch1');
   const { id }: { id: string } = ctx.params;
   if (!ObjectID.isValid(id)) {
-    throw new BadRequest({ message: '아이디 타입 오류' });
+    throw new BadRequest({ message: '아이디 타입 오류', location: 'middleware.checkObjectid' });
   }
   return next();
 };
