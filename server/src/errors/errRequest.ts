86bf3928ff9b9f6ffc962798c053e9bf24ba3d46
euntaek <zmn5119@gmail.com>
2020-10-04 02:45:00 +0900

    feat(errReuqest): 401 Unauthorized 추가

--- server/src/errors/errRequest.ts
+++ server/src/errors/errRequest.ts
@@ -15,7 +15,7 @@ export abstract class RequestError extends Error {
 }
 export class InternalServerError extends RequestError {
   constructor(errorParams?: ErrorParmas) {
-    super(errorParams?.message || '내부 서버 오류');
+    super(errorParams?.message || '내부 서버 오류입니다.');
     this.log = errorParams?.log;
     this.location = errorParams?.location;
     this.statusCode = StatusCodes.INTERNAL_SERVER_ERROR;
@@ -23,7 +23,7 @@ export class InternalServerError extends RequestError {
 }
 export class BadRequest extends RequestError {
   constructor(errorParams?: ErrorParmas) {
-    super(errorParams?.message || '유효하지 않은 요청');
+    super(errorParams?.message || '잘못 된 요청입니다.');
     this.log = errorParams?.log;
     this.location = errorParams?.location;
     this.statusCode = StatusCodes.BAD_REQUEST;
@@ -32,7 +32,7 @@ export class BadRequest extends RequestError {
 
 export class NotFound extends RequestError {
   constructor(errorParams?: ErrorParmas) {
-    super(errorParams?.message || '리소스가 존재하지 않음');
+    super(errorParams?.message || '리소스가 존재하지 않습니다.');
     this.log = errorParams?.log;
     this.location = errorParams?.location;
     this.statusCode = StatusCodes.NOT_FOUND;
@@ -45,5 +45,13 @@ export class Conflict extends RequestError {
     this.log = errorParams?.log;
     this.location = errorParams?.location;
     this.statusCode = StatusCodes.CONFLICT;
+  }
+}
+export class Unauthorized extends RequestError {
+  constructor(errorParams?: ErrorParmas) {
+    super(errorParams?.message || '권한이 없습니다.');
+    this.log = errorParams?.log;
+    this.location = errorParams?.location;
+    this.statusCode = StatusCodes.UNAUTHORIZED;
   }
 }
