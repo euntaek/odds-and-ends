848fbd2d9a19f0cf18ed8dd9f5f10df11c5bdc0f
euntaek <zmn5119@gmail.com>
2020-10-13 03:34:08 +0900

    recactor(typeorm): db 연결 옵션 리팩토링

--- src/database.ts
+++ src/database.ts
@@ -1,11 +1,40 @@
+import { createConnection, getConnection, ConnectionOptions } from 'typeorm';
 import 'reflect-metadata';
 import 'pg';
-import { createConnection, getConnection } from 'typeorm';
+
+import {
+  TYPEORM_TYPE,
+  TYPEORM_HOST,
+  TYPEORM_PORT,
+  TYPEORM_USERNAME,
+  TYPEORM_PASSWORD,
+  TYPEORM_DATABASE,
+  TYPEORM_SYNCHRONIZE,
+  TYPEORM_LOGGING,
+  TYPEORM_DROPSCHEMA,
+  TYPEORM_ENTITIES,
+  TYPEORM_MIGRATIONS,
+  TYPEORM_SUBSCRIBERS,
+} from './constans';
 
 export default {
   async connection(): Promise<void> {
+    const connectionOptions: ConnectionOptions = {
+      type: TYPEORM_TYPE as any,
+      host: TYPEORM_HOST,
+      port: TYPEORM_PORT,
+      username: TYPEORM_USERNAME,
+      password: TYPEORM_PASSWORD,
+      database: TYPEORM_DATABASE,
+      synchronize: TYPEORM_SYNCHRONIZE,
+      logging: TYPEORM_LOGGING,
+      dropSchema: TYPEORM_DROPSCHEMA,
+      entities: [TYPEORM_ENTITIES],
+      migrations: [TYPEORM_MIGRATIONS],
+      subscribers: [TYPEORM_SUBSCRIBERS],
+    };
     try {
-      await createConnection();
+      await createConnection(connectionOptions);
       console.log('Databae connected');
     } catch (e) {
       throw new Error(e);
