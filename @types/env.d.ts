723913cc411d3febd505dc4a488dce924540732b
euntaek <zmn5119@gmail.com>
2020-10-13 03:25:31 +0900

    update(env): 환경변수 이름 변경, 항목 및 타입 추가

--- @types/env.d.ts
+++ @types/env.d.ts
@@ -1,26 +1,36 @@
 declare namespace NodeJS {
   export interface ProcessEnv {
-    NODE_ENV: 'development' | 'production'
-    PORT: string
-    SECRET_KEY: string
-    TYPEORM_TYPE: string
-    TYPEORM_HOST: string
-    TYPEORM_PORT: string
-    TYPEORM_USERNAME: string
-    TYPEORM_PASSWORD: string
-    TYPEORM_DATABASE: string
-    TYPEORM_SYNCHRONIZE: string
-    TYPEORM_LOGGING: string
-    TYPEORM_DROPSCHEMA: string
-    TYPEORM_ENTITIES: string
-    TYPEORM_MIGRATIONS: string
-    TYPEORM_SUBSCRIBERS: string
+    NODE_ENV: 'development' | 'production';
+    PORT: string;
 
-    SMTPT_SERVICE: string
-    SMTPT_HOST: string
-    SMTPT_PORT: string
-    SMTPT_SECURE: string
-    SMTPT_USER: string
-    SMTPT_PASSWORD: string
+    ACCESS_TOKEN_SECRET: string;
+    REFRESH_TOKEN_SECRET: string;
+    ACCESS_TOKEN_EXPIRES_IN: string;
+    REFRESH_TOKEN_EXPIRES_IN: string;
+
+    TYPEORM_TYPE: string;
+    TYPEORM_HOST: string;
+    TYPEORM_PORT: string;
+    TYPEORM_USERNAME: string;
+    TYPEORM_PASSWORD: string;
+    TYPEORM_DATABASE: string;
+    TYPEORM_SYNCHRONIZE: string;
+    TYPEORM_LOGGING: string;
+    TYPEORM_DROPSCHEMA: string;
+    TYPEORM_ENTITIES: string;
+    TYPEORM_MIGRATIONS: string;
+    TYPEORM_SUBSCRIBERS: string;
+
+    REDIS_HOST: string;
+    REDIS_PORT: string;
+    REDIS_DB: string;
+    REDIS_PASSWORD: string;
+
+    SMTPT_SERVICE: string;
+    SMTPT_HOST: string;
+    SMTPT_PORT: string;
+    SMTPT_SECURE: string;
+    SMTPT_USER: string;
+    SMTPT_PASSWORD: string;
   }
 }
