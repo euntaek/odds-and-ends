8b3c276c0ec2d8cfb9a5051177c4aa076e5c465d
euntaek <zmn5119@gmail.com>
2020-10-14 18:51:43 +0900

    Update(types): 커스텀 타입 폴더 위치를 root에서 src 안으로 변경

--- @types/joi-objectid/index.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'joi-objectid' {
-  import { Root, StringSchema } from 'joi';
-
-  type JoiStringSchema = () => StringSchema;
-
-  export default function (Joi: Root, message?: string): JoiStringSchema;
-}
