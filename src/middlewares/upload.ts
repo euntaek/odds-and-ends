dd7b3c0ffd045eb3241a84379cb79955adfed709
euntaek <zmn5119@gmail.com>
2021-03-29 23:35:32 +0900

    move: checkUser, checkFollow 함수 미들웨어로 이동

--- src/middlewares/upload.ts
+++ src/middlewares/upload.ts
@@ -2,15 +2,19 @@ import { Middleware } from '@koa/router';
 import multer from '@koa/multer';
 
 import { s3Storage } from '@/utils/s3';
+import { InternalServerError } from '@/errors/errRequest';
 
 const limits = { fileSize: 4 * 1024 * 1024 }; // 4MB
 
 export function upload(fieldName: string, type: 'single' | 'array'): Middleware {
   return async (ctx, next) => {
-    const { id } = ctx.state.user;
-    await multer({ storage: s3Storage(id, fieldName) as any, limits })[type](fieldName)(
-      ctx as any,
-      next,
-    );
+    try {
+      const { id } = ctx.state.user;
+      const multerInstance = multer({ storage: s3Storage(id, fieldName) as any, limits })[type];
+      // 이미지 업로드
+      await multerInstance(fieldName)(ctx as any, next);
+    } catch (error) {
+      ctx.throw(new InternalServerError({ ...error, name: 'AWS_S3_UPLOAD_ERROR' }));
+    }
   };
 }
