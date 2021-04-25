62db7fce6cb5875e1638fa5cf131ad319b4be24b
euntaek <zmn5119@gmail.com>
2021-04-25 20:03:14 +0900

    [ADD]: 유저페이지 라우터 접두사 추가

--- client/next.config.js
+++ client/next.config.js
@@ -3,7 +3,23 @@ module.exports = {
     webpack5: true,
   },
   i18n: {
-    locales: ["ko-KR"],
-    defaultLocale: "ko-KR",
+    locales: ['ko-KR'],
+    defaultLocale: 'ko-KR',
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/@:username',
+        destination: '/users/profile',
+      },
+      {
+        source: '/@:username/following',
+        destination: '/users/following',
+      },
+      {
+        source: '/@:username/followers',
+        destination: '/users/followers',
+      },
+    ];
   },
 };
