b09c80c28d8727b231438809888b35d5bc9d711a
euntaek <zmn5119@gmail.com>
2021-07-22 21:17:33 +0900

    [STUDY] jest 간단하게 공부

---
+++ study/JestJS/js/index.js
@@ -0,0 +1,20 @@
+const sum = (a, b) => a + b;
+
+const person = (name, age) => ({ name, age });
+
+const toggle = (a) => !a;
+
+const range = (start, end) => {
+  let result = [];
+  for (let i = start; i <= end; i++) {
+    result.push(i);
+  }
+  return result;
+};
+
+module.exports = {
+  sum,
+  person,
+  toggle,
+  range,
+};
