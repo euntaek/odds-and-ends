469f24ef78a9bc8eb1438a422dc6671a0be987fe
euntaek <zmn5119@gmail.com>
2021-09-16 18:57:00 +0900

    [STUDY] NestJs로 게시판 만들기 02

--- study/NestJS/nestjs-board-app/src/boards/boards.controller.ts
+++ study/NestJS/nestjs-board-app/src/boards/boards.controller.ts
@@ -1,7 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { Board } from './board.model';
 
 import { BoardsService } from './boards.service';
+import { CreateBoardDto } from './dto/create-board.dto';
 
 @Controller('boards')
 export class BoardsController {
@@ -10,5 +19,28 @@ export class BoardsController {
   @Get()
   getAllBoard(): Board[] {
     return this.boardsService.getAllBoards();
+  }
+
+  @Post()
+  createBoard(@Body() createBoardDto: CreateBoardDto): Board {
+    return this.boardsService.createBoard(createBoardDto);
+  }
+
+  @Get('/:id')
+  getBoardById(@Param('id') id: string) {
+    return this.boardsService.getBoardById(id);
+  }
+
+  @Delete('/:id')
+  deleteBoard(@Param('id') id: string) {
+    this.deleteBoard(id);
+  }
+
+  @Patch('/:id')
+  updateBoardStatus(
+    @Param('id') id: string,
+    @Body('status') status: 'PUBLIC' | 'PRIVATE',
+  ) {
+    return this.boardsService.updateBoardStatus(id, status);
   }
 }
--- study/NestJS/nestjs-board-app/src/boards/boards.service.ts
+++ study/NestJS/nestjs-board-app/src/boards/boards.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@nestjs/common';
+import { v1 as uuid } from 'uuid';
+
 import { Board } from './board.model';
+import { CreateBoardDto } from './dto/create-board.dto';
 
 @Injectable()
 export class BoardsService {
@@ -7,5 +10,30 @@ export class BoardsService {
 
   getAllBoards(): Board[] {
     return this.boards;
+  }
+  createBoard(createBoardDto: CreateBoardDto) {
+    const { title, description } = createBoardDto;
+    const board: Board = {
+      id: uuid(),
+      title,
+      description,
+      status: 'PUBLIC',
+    };
+    this.boards.push(board);
+    return board;
+  }
+
+  getBoardById(id: string) {
+    return this.boards.find((board) => board.id === id);
+  }
+
+  deleteBoard(id: string) {
+    this.boards = this.boards.filter((board) => board.id !== id);
+  }
+
+  updateBoardStatus(id: string, status: 'PUBLIC' | 'PRIVATE') {
+    const board = this.getBoardById(id);
+    board.status = status;
+    return board;
   }
 }
